f5b7ef42366fd39152fb3ef25550e06a644d49bf
Anil Kumar <Anil3.Kumar@igglobal.com>
2019-12-11 11:14:13 +0530

    add build file

---
+++ build/precache-manifest.830c6b09f60a560d1783e5b924bd4142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60712624055adf653b205e41260286cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc52d1fc4b81e05a5d65",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "1262a7331871cad8818d",
+    "url": "/static/js/2.189d1538.chunk.js"
+  },
+  {
+    "revision": "bc52d1fc4b81e05a5d65",
+    "url": "/static/js/main.5df951e3.chunk.js"
+  },
+  {
+    "revision": "6dbe56ce70542469ff0d",
+    "url": "/static/js/runtime-main.04784543.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
